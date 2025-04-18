f4c18586ae4f80ad6e564351df15f93de5e4e1bd
roll_out <128191883+rollouts21@users.noreply.github.com>
2025-04-18 19:57:38 +1000

    q

--- telegram_sender.py
+++ telegram_sender.py
@@ -3,7 +3,7 @@ import socketserver
 import json
 import requests
 import logging
-
+import cgi
 
 logging.basicConfig(
     filename="/var/log/telegram_sender.log",
@@ -25,59 +25,63 @@ class TelegramHandler(http.server.BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
+            # Установите заголовок CORS в начале обработки запроса
             self.send_header("Access-Control-Allow-Origin", "*")
-            content_length = int(self.headers["Content-Length"])
-            post_data = self.rfile.read(content_length)
-            data = json.loads(post_data.decode("utf-8"))
+            ctype, pdict = cgi.parse_header(self.headers.get("Content-Type"))
+            if ctype == "application/json":
+                content_length = int(self.headers.get("Content-Length"))
+                post_data = self.rfile.read(content_length)
+                data = json.loads(post_data.decode("utf-8"))
 
-            name = data.get("name")
-            email = data.get("email")
-            phone = data.get("phone")
-            comment = data.get("comment")
+                name = data.get("name")
+                email = data.get("email")
+                phone = data.get("phone")
+                comment = data.get("comment")
 
-            bot_token = "7639197628:AAG3_sbUb2hs6xSoHmruMv_kteFvw2fBASk"  # Замените на токен вашего бота
-            chat_id = "820559840"  # Замените на ID чата
+                bot_token = "7639197628:AAG3_sbUb2hs6xSoHmruMv_kteFvw2fBASk"  # Замените на токен вашего бота
+                chat_id = "820559840"  # Замените на ID чата
 
-            message = f"Новая заявка:\nИмя: {name}\nEmail: {email}\nТелефон: {phone}\nКомментарий: {comment}"
-            url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={message}"
+                message = f"Новая заявка:\nИмя: {name}\nEmail: {email}\nТелефон: {phone}\nКомментарий: {comment}"
+                url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={message}"
 
-            try:
-                logging.info(f"Отправка сообщения в Telegram: {url}")  # Log the URL
-                response = requests.get(url)
-                response.raise_for_status()
-                self.send_response(200)
-                self.send_header("Content-type", "application/json")
-                self.end_headers()
-                self.wfile.write(
-                    bytes(
-                        json.dumps(
-                            {
-                                "status": "success",
-                                "message": "Уведомление успешно отправлено!",
-                            }
-                        ),
-                        "utf-8",
+                try:
+                    logging.info(f"Отправка сообщения в Telegram: {url}")  # Log the URL
+                    response = requests.get(url)
+                    response.raise_for_status()
+                    self.send_response(200)
+                    self.send_header("Content-type", "application/json")
+                    self.end_headers()
+                    self.wfile.write(
+                        bytes(
+                            json.dumps(
+                                {
+                                    "status": "success",
+                                    "message": "Уведомление успешно отправлено!",
+                                }
+                            ),
+                            "utf-8",
+                        )
                     )
-                )
-                logging.info("Сообщение успешно отправлено в Telegram!")
+                    logging.info("Сообщение успешно отправлено в Telegram!")
 
-            except requests.exceptions.RequestException as e:
-                self.send_response(500)
-                self.send_header("Content-type", "application/json")
-                self.end_headers()
-                self.wfile.write(
-                    bytes(
-                        json.dumps(
-                            {
-                                "status": "error",
-                                "message": f"Ошибка при отправке уведомления в Telegram: {str(e)}",
-                            }
-                        ),
-                        "utf-8",
+                except requests.exceptions.RequestException as e:
+                    self.send_response(500)
+                    self.send_header("Content-type", "application/json")
+                    self.end_headers()
+                    self.wfile.write(
+                        bytes(
+                            json.dumps(
+                                {
+                                    "status": "error",
+                                    "message": f"Ошибка при отправке уведомления в Telegram: {str(e)}",
+                                }
+                            ),
+                            "utf-8",
+                        )
                     )
-                )
-                logging.error(f"Ошибка при отправке уведомления в Telegram: {str(e)}")
-
+                    logging.error(
+                        f"Ошибка при отправке уведомления в Telegram: {str(e)}"
+                    )
         except Exception as e:
             logging.exception(f"Общая ошибка при обработке POST-запроса: {e}")
 
